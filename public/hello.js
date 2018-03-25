54b2e16a0c295d62d146de6062cf170ab7819750
Piotr <karager009@gmail.com>
2018-03-25 23:18:50 +0200

    first page

--- public/hello.js
+++ public/hello.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
     $.ajax({
         url: "http://localhost:8080/greeting"
     }).then(function(data, status, jqxhr) {
-       $('.greeting-id').append(data.id);
-       $('.greeting-content').append(data.content);
+       //$('.greeting-id').append(data.id);
+       //$('.greeting-content').append(data.content);
        console.log(jqxhr);
     });
 });
@@ -29,5 +29,29 @@ function wyslij()
         $('.greeting-content').append(data.content);
         console.log(jqxhr);
     });
+
+    potrzebaJeszczeImie();
+}
+
+
+function potrzebaJeszczeImie(){
+    $('#imie_div').append("<label for=\"imie\">Podaj imię:</label>\n" +
+        "<input type=\"text\" class=\"form-control\" id=\"imie\">" +
+    "<br>");
+
+    $('#send').attr("onclick", "wyslijZImieniem()");
+}
+
+function wyslijZImieniem(){
+    //document.write("wysłano: " + $("#nazwisko").val() + ", "+ $("#imie").val());
+nieMaTakiegoUzytkownika();
+
+}
+
+function nieMaTakiegoUzytkownika(){
+    $("#main").prepend("<h2>Podałeś złe dane!</h2>");
+
+    $("#nazwisko").val('');
+    $('#imie').val('');
 
 }
