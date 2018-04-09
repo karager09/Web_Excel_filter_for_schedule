cb746d4b76cc0eb30f193f55fc007322c357109a
Adamek7 <adam.buczek17@gmail.com>
2018-04-09 14:33:55 +0200

    authorization

--- src/main/java/server/SearchController.java
+++ src/main/java/server/SearchController.java
@@ -20,26 +20,6 @@ public class SearchController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    //@CrossOrigin(origins = "http://localhost:9000")
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
-        System.out.println("==== in greeting ====");
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
-
-//    @GetMapping("/greeting-javaconfig")
-//    public Greeting greetingWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
-//        System.out.println("==== in greeting ====");
-//        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-//    }
-
-    //@CrossOrigin(origins = "http://localhost:9000")
-    @PostMapping(value = "/greeting")
-    public Greeting greetingResponse(@RequestBody Greeting greeting) {
-        System.out.println("==== in greeting2 ====");
-        return new Greeting(greeting.getId()+9, greeting.getContent()+", tak.");
-    }
-
     @PostMapping(value = "/api/search")
     public boolean searchResponse(@RequestBody Lecturer lecturer) throws IOException, InvalidFormatException {
         System.out.println("Searching");
