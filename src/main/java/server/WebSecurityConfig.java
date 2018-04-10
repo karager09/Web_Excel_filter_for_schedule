514d1f27513783478eaa1df8d3bf6c0ebd8495c1
Piotr <karager009@gmail.com>
2018-04-10 15:15:40 +0200

    login

--- src/main/java/server/WebSecurityConfig.java
+++ src/main/java/server/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/index", "/js/hello.js", "/api/**").permitAll()
+                .antMatchers("/", "/index", "/js/hello.js", "/api/**", "/js/admin.js").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
